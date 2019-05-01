ce97a47577adae469c9dc3c292afe3e2b8530ee6
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-05-01 16:28:23 -0400

    Added demo bdg files
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ R/demo.R
@@ -0,0 +1,23 @@
+#' Get a bedGraph file for demo
+#' 
+#' @return The path to a valid bdg file
+#' 
+#' @examples
+#' bdg <- get_demo_bdg()
+#'
+#' @export
+get_demo_bdg <- function() {
+	system.file("extdata/demo.bdg", package="heatmaps", mustWork = TRUE)
+}
+
+#' Get an indexed bedGraph file for demo
+#' 
+#' @return The path to a valid indexed bdg file
+#' 
+#' @examples
+#' bdg <- get_demo_bdg_idx()
+#'
+#' @export
+get_demo_bdg_idx <- function() {
+	system.file("extdata/demo.bdg.gz", package="heatmaps", mustWork = TRUE)
+}
