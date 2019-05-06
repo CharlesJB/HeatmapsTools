a9905f4edb556b65795a1fda5bdad0072ef80eff
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2019-05-06 17:01:30 -0400

    Added the get_demo_bed_files function
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/demo.R
+++ R/demo.R
@@ -21,3 +21,17 @@ get_demo_bdg <- function() {
 get_demo_bdg_idx <- function() {
     system.file("extdata/demo.bdg.gz", package="heatmaps", mustWork = TRUE)
 }
+
+#' Get a bed file for demo
+#'
+#' @return The path to a valid bed file
+#'
+#' @examples
+#' bed <- get_demo_bed_files()
+#'
+#' @export
+get_demo_bed_files <- function() {
+    c(system.file("extdata/demo.bed", package="heatmaps", mustWork = TRUE),
+      system.file("extdata/demo2.bed", package="heatmaps", mustWork = TRUE))
+
+}
